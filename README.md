062d238b8865ba5f60a76b01717444ab54ed4a12
Ralph Schiedon <drZymo@users.noreply.github.com>
2024-02-20 22:18:03 +0100

    Replace C runtime functions with Windows API calls
    This will make the executable significantly smaller (6.5K vs 130K).

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Waits for key press to restore resolution.
 1. Edit defines `GAME_WIDTH`, `GAME_HEIGHT`, and `GAME_EXE` in [launcher.c](launcher.c).
 2. Open `Developer Command Prompt for VS 2022` and go to this folder.
 3. Compile: `cl /c /Wall launcher.c`
-4. Link: `link /out:launcher.exe launcher.obj user32.lib`
+4. Link: `link /machine:X64 /subsystem:console /entry:Main /out:launcher.exe launcher.obj kernel32.lib user32.lib shell32.lib`
--- launcher.c
+++ launcher.c
@@ -1,25 +1,37 @@
 #define WIN32_LEAN_AND_MEAN
-#include <stdio.h>
-#include <stdlib.h>
 #include <windows.h>
+#include <shellapi.h>
 
 #define GAME_WIDTH      1920
 #define GAME_HEIGHT     1080
 #define GAME_EXE        L"D:\\GamePassGames\\Starfield\\Content\\Starfield.exe"
 
-int main(void)
+HANDLE stdout;
+HANDLE stdin;
+
+static BOOL InitConsole(void);
+static void Log(const wchar_t* message, ...);
+
+void Main(void)
 {
-    wprintf(L"Getting current display settings ...\n");
+    if (!InitConsole())
+    {
+        ExitProcess(1);
+        return;
+    }
+    
+    Log(L"Getting current display settings ...\n");
     DEVMODEW original_mode = {0};
     BOOL enum_result = EnumDisplaySettingsW(NULL, ENUM_CURRENT_SETTINGS, &original_mode);
     if (!enum_result)
     {
-        wprintf(L"Get current display settings failed.\n");
-        return 1;
+        Log(L"Get current display settings failed.\n");
+        ExitProcess(2);
+        return;
     }
-    wprintf(L"Current display settings: %lix%li\n", original_mode.dmPelsWidth, original_mode.dmPelsHeight);
+    Log(L"Current display settings: %1!i!x%2!i!\n", original_mode.dmPelsWidth, original_mode.dmPelsHeight);
 
-    wprintf(L"Changing display settings to %ix%i ...\n", GAME_WIDTH, GAME_HEIGHT);
+    Log(L"Changing display settings to %1!i!x%2!i! ...\n", GAME_WIDTH, GAME_HEIGHT);
     DEVMODEW new_mode = {0};
     new_mode.dmSize = sizeof(DEVMODEW);
     new_mode.dmFields = DM_PELSWIDTH | DM_PELSHEIGHT;
@@ -28,23 +40,49 @@ int main(void)
     LONG change_result = ChangeDisplaySettingsW(&new_mode, 0);
     if (change_result != DISP_CHANGE_SUCCESSFUL)
     {
-        wprintf(L"Change display settings failed.\nERROR: %li\n", change_result);
-        return 1;
+        Log(L"Change display settings failed.\nERROR: %1!i!\n", change_result);
+        ExitProcess(3);
+        return;
     }
 
-    wprintf(L"Starting process ...\n");
-    _wsystem(GAME_EXE);
+    Log(L"Starting process ...\n");
+    ShellExecuteW(NULL, NULL, GAME_EXE, NULL, NULL, SW_NORMAL);
 
-    wprintf(L"Press any key to restore display settings ...\n");
-    getwchar();
+    Log(L"Press ENTER to restore display settings ...\n");
+    wchar_t buffer[2] = {0};
+    DWORD chars_read;
+    ReadConsoleW(stdin, buffer, 1, &chars_read, NULL);
 
-    wprintf(L"Restoring display settings ...\n");
+    Log(L"Restoring display settings ...\n");
     LONG restore_result = ChangeDisplaySettingsW(&original_mode, 0);
     if (restore_result != DISP_CHANGE_SUCCESSFUL)
     {
-        wprintf(L"Restore display settings failed.\nERROR: %li\n", restore_result);
-        return 1;
+        Log(L"Restore display settings failed.\nERROR: %1!i!\n", restore_result);
+        ExitProcess(4);
+        return;
     }
 
-    return 0;
+    ExitProcess(0);
+    return;
+}
+
+BOOL InitConsole(void)
+{
+    stdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (stdout == INVALID_HANDLE_VALUE || stdout == NULL) return FALSE;
+    stdin = GetStdHandle(STD_INPUT_HANDLE);
+    if (stdin == INVALID_HANDLE_VALUE || stdin == NULL) return FALSE;
+    return TRUE;
+}
+
+void Log(const wchar_t* message, ...)
+{
+    wchar_t buffer[1024];
+
+    va_list args;
+    va_start(args, message);
+    DWORD count = FormatMessageW(FORMAT_MESSAGE_FROM_STRING, message, 0, 0, buffer, 1024, &args);
+    va_end(args);
+
+    WriteConsoleW(stdout, buffer, count, NULL, NULL);
 }
